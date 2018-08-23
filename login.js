bab9c5f5386d81d413ae4c01f31c128f882329ae
Cristina <barcelona1252@gmail.com>
2018-08-23 13:41:45 +0100

    front extension actualizado

---
+++ login.js
@@ -0,0 +1,27 @@
+async function login(){
+  const data = {
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value
+  }
+
+  axios.post('http://localhost:3333/api/sign', data)
+  .then(function (response) {
+    // handle success
+    if(response.status === 200){
+      chrome.storage.local.set({'email': data.email, 'password': data.password, 'id': response.data._id}, function() {
+        window.location.href = "/subs.html";
+      });
+    }
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+}
+$("#login").on("click", function(){
+  login();
+})
